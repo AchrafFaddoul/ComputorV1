0834842d9635a7db9965c3622a745321d4a38a57
afaddoul <faddoulachraf39@gmail.com>
2020-11-21 23:50:16 +0100

    create coeff_ modifier and reduced_form_generator functions

--- equation_solver.py
+++ equation_solver.py
@@ -1,6 +1,33 @@
 def deg_extractor(equation):
     return equation[0]['expo']
 
+def coeff_modifier(coeff, deg):
+    string = ''
+    if coeff < 0:
+        coeff *= -1
+        if deg != 0:
+            string = '- '
+        else:
+            string = '-'
+    else:
+        if deg != 0:
+            string = '+ '
+        else:
+            string = '+'
+    string += str(int(coeff)) if coeff % 10 else str(coeff)
+    string += ' '
+    return string
+
+
+def reduced_form_generator(equation):
+    reduced_polynomial = ''
+    equation_rev = [elm for elm in reversed(equation)]
+    for elm in equation_rev:
+        reduced_polynomial += coeff_modifier(elm['coeff'], elm['expo'])
+    print(reduced_polynomial)
+        
+
 def equation_solver(equation):
-    print(deg_extractor(equation))
-    
+    deg = deg_extractor(equation)
+    print(equation)
+    reduced_form_generator(equation)
