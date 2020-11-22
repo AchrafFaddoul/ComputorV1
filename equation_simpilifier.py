f2e56dd573e6e246eb9c0230cab82b2841ef7cb8
afaddoul <faddoulachraf39@gmail.com>
2020-11-22 04:06:38 +0100

    fix poping elms while iterating on list issue by creating new list

--- equation_simpilifier.py
+++ equation_simpilifier.py
@@ -5,16 +5,21 @@ def equation_simplifier(equation):
     for elm in equation[1]:
         elm['coeff'] = elm['coeff'] * -1
         equation[0].append(elm)
-        if len(equation[1]) > 0:
-            equation[1].pop(i)
         i += 1
     equation[0] = sorted(equation[0], key=lambda k: k['expo'], reverse=True)
+    #for elm in equation[0]:
+     #   if not elm['coeff']:
+      #      equation[0].pop(equation[0].index(elm))
+    equation[0] = [elm for elm in equation[0] if elm['coeff']]
     for i in equation[0]:
         for j in equation[0]:
             if j['expo'] == i['expo'] and equation[0].index(i) != equation[0].index(j):
+                print('bef',i['coeff'])
                 i['coeff'] += j['coeff']
+                print('aft',i['coeff'])
                 if len(equation[0]) > 0:
                     equation[0].pop(equation[0].index(j))
+    #print('++++', equation[0], equation[1])
     coeff_lst = [elm['coeff'] for elm in equation[0]]
     gcd = find_gcd(coeff_lst)
     if (gcd > 1):
@@ -22,6 +27,5 @@ def equation_simplifier(equation):
             elm['coeff'] = elm['coeff'] / gcd
     if len(equation) == 2:
         equation.pop(1)
-        equation = equation[0]    
-    print(equation)
+        equation = equation[0]
     return equation
